5b4752e3b14d5cb93c031eb6184a39a78a9de539
anggialberto <albertoanggi@gmail.com>
2019-03-02 01:48:25 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,28 +1,64 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
+
 
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      tinggiBadan: 0,
+      siswa: [
+        { id: 1, nama: "Anggi", kelas: "3IA22" },
+        { id: 2, nama: "Alberto", kelas: "3AI22" },
+        { id: 3, nama: "Simarmata", kelas: "3IA22" }
+      ],
+      planets: []
+    }
+  }
+
+  _handleChange = (e) => {
+    this.setState({ [e.target.name]: Number(e.target.value) })
+  }
+
+  _handleClick = () => {
+    axios.get("https://swapi.co/api/planets")
+      .then(res => {
+        this.setState({ planets: res.data.results })
+      })
+      .catch(err => {
+        console.error(err)
+      });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <button onClick={this._handleClick}>Tampilkan Planet</button>
+        <h1>List Data Star Wars</h1>
+        <table border="1px">
+          <thead>
+            <th>gravity</th>
+            <th>name</th>
+            <th>terrain</th>
+          </thead>
+          <tbody>
+            {
+              this.state.planets.map((data, index) => {
+                return (
+                  <tr>
+                    <td>{data.gravity}</td>
+                    <td>{data.name}</td>
+                    <td>{data.terrain}</td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
