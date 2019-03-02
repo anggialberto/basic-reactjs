eefbc2e099e98fa7953f4bc8bbdbc0162641f7d1
anggialberto <albertoanggi@gmail.com>
2019-03-02 09:58:26 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
+import Table from './components/Table'
+
 
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      planets: []
+    }
+  }
+
+  _handleChange = (e) => {
+    this.setState({ [e.target.name]: Number(e.target.value) })
+  }
+
+  _handleClick = () => {
+    axios.get("https://swapi.co/api/planets")
+      .then(res => {
+        this.setState({ planets: res.data.results })
+      })
+      .catch(err => {
+        console.error(err)
+      });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <button onClick={this._handleClick}>Tampilkan Planet</button>
+        <h1>List Data Star Wars</h1>
+        <Table planets={this.state.planets} />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
