8710aca28a015dac82f2c530af3600b4bef594f7
anggialberto <albertoanggi@gmail.com>
2019-03-02 18:38:42 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,28 +1,61 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      siswa: [
+        { id: 1, nama: "Anggi", kelas: "3IA22" },
+        { id: 2, nama: "Alberto", kelas: "3IA22" },
+        { id: 3, nama: "Simarmata", kelas: "3IA22" },
+
+      ],
+      judul: "List Data Siswa"
+    }
+  }
+
+  componentDidMount() {
+    // Ketika method render berhasil dijalankan
+    // alert("Hello World")
+  }
+
+  componentDidUpdate() {
+    // Ketika ada perubahan state atau props
+    alert("componentDidUpdate")
+  }
+
+  componentDidUmount() {
+    // Ketika component akan di hapus / ditiadakan
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <div>
+        <h1>{this.state.judul}</h1>
+        <table>
+          <th>ID</th>
+          <th>NAMA</th>
+          <th>KELAS</th>
+          <tbody>
+            {
+              this.state.siswa.map(value => {
+                return (
+                  <tr>
+                    <td>{value.id}</td>
+                    <td>{value.nama}</td>
+                    <td>{value.kelas}</td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
+        <button onClick={() => this.setState({ judul: "List Data Berubah" })}>
+          Click me!
+        </button>
+      </div >
+    )
   }
 }
 
-export default App;
+export default App
